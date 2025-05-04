5ec1417de68ca190b9690edfe4501f0057f47961
karukan029 <karukan029@gmail.com>
2025-05-04 10:45:49 +0900

    chore: add remote image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com'],
+    remotePatterns: [new URL('https://images.pexels.com/photos/1181271/**')],
+  },
+};
 
 module.exports = nextConfig;
